3941cffdc5195bcee547b2c526f21d315852a765
reyhanhaidar <azanireyhan@student.telkomuniversity.ac.id>
2023-04-18 17:12:36 +0700

    menambahkan mahasiswa

---
+++ Mod9_1302210110/Mod9_1302210110/Controllers/MahasiswaController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mod9_1302210110.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+
+        public static int Year = 2019;
+
+        public MahasiswaController() { }
+
+        static List<string> Course = new List<string>()
+        {
+            new string("pbo"),
+            new string("basdar"),
+            new string("kpl")
+        };
+
+        static List<Mahasiswa> list = new List<Mahasiswa>
+        {
+
+            new Mahasiswa("Reyhan",  1302210110, Year, Course),
+            new Mahasiswa("Yosua",  1302210126, Year, Course),
+            new Mahasiswa("Andry", 1302213005, Year, Course),
+            new Mahasiswa("Rafa",  1302210123, Year, Course)
+        };
+
+
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return (IEnumerable<string>)list;
+        }
+        [HttpGet("id")]
+        public IEnumerable<string> Get(int id)
+        {
+            return (IEnumerable<string>)list[id];
+        }
+
+        [HttpPost]
+        public void Post(Mahasiswa lt)
+        {
+          list.Add(lt);
+        }
+        [HttpDelete("id")]
+        public void Delete(int id)
+        {
+            list.RemoveAt(id);
+        }
+
+    }
+}
---
+++ Mod9_1302210110/Mod9_1302210110/Mahasiswa.cs
@@ -0,0 +1,33 @@
+﻿namespace Mod9_1302210110
+{
+    public class Mahasiswa
+    {
+
+        public String nama { get; set; }
+
+
+
+        public int nim { get; set; }
+
+
+
+        public List<String> Course { get; set; }
+
+
+
+        public int Year { get; set; }
+
+
+
+        public Mahasiswa(string v, int nim, int Year, List<string> Course) { 
+            this.nama = v;
+            this.nim = nim;
+            this.Course = Course;
+            this.Year = Year;   
+        }
+
+        
+
+     
+    }
+}
